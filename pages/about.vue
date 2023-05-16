a3723adec7b76bc93c7e2d997755a434a0c77d2d
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-16 20:28:04 -0300

    adiciona header com nuxtlinks

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,17 @@
 <template>
+	<div>
+		<header>
+			<nav>
+				<NuxtLink to="/">Nuxt Dojo</NuxtLink>
+				<ul>
+					<li><NuxtLink to="/">Home</NuxtLink></li>
+					<li><NuxtLink to="/about">About</NuxtLink></li>
+					<li><NuxtLink to="/products">Products</NuxtLink></li>
+					<a href="/about">normal link to about page</a>
+				</ul>
+			</nav>
+		</header>
+	</div>
 	<h2>About</h2>
 	<p>Lorem ipsum dolore est laborum dolor velit non proident consequat commodo. Ex laboris deserunt incididunt amet in in mollit mollit in dolor.</p>
 	<p>Lorem ipsum dolore est laborum dolor velit non proident consequat commodo. Ex laboris deserunt incididunt amet in in mollit mollit in dolor.</p>

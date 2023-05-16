6b9a0a16608845f10d8f9f15bd3c20dc3b964af2
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-16 19:38:02 -0300

    cria projeto nuxt

--- pages/about.vue
+++ pages/about.vue
@@ -1,1 +1,5 @@
-<template>About</template>
+<template>
+<h2>About</h2>
+<p>Lorem ipsum deserunt ut commodo pariatur excepteur officia sit est commodo et. Tempor in veniam in in dolor et est aliquip adipisicing ad est quis ullamco in.</p>
+</template>
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,5 @@
-<template>About</template>
+<template>
+	<h1>Home</h1>
+	<p>Lorem ipsum dolore est laborum dolor velit non proident consequat commodo. Ex laboris deserunt incididunt amet in in mollit mollit in dolor. Occaecat ea elit officia eu eu eu sunt cupidatat. Ad dolore sint ea dolor minim dolor amet dolor laboris consectetur quis consectetur.</p>
+</template>
+<style scoped></style>

c65a2c719a0bb08db578b26dd43a3582e5ca89ea
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-16 20:11:24 -0300

    adiciona pagina de produto
    captura o parâmetro da rota

---
+++ pages/products/[id].vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<p>Product details for {{ id }}</p>
+		<p>Id laborum excepteur sint do aliquip exercitation pariatur velit pariatur aliquip.</p>
+	</div>
+</template>
+<script setup>
+	const { id } = useRoute().params
+</script>

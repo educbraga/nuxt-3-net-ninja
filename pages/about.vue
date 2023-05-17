b9195ba50ba85e9f4f508c93267f62cc041ab8ed
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-17 00:10:21 -0300

    adiciona roteamento de servidor

--- pages/about.vue
+++ pages/about.vue
@@ -2,7 +2,11 @@
 	<h2>About</h2>
 	<p>Lorem ipsum dolore est laborum dolor velit non proident consequat commodo. Ex laboris deserunt incididunt amet in in mollit mollit in dolor.</p>
 	<p>Lorem ipsum dolore est laborum dolor velit non proident consequat commodo. Ex laboris deserunt incididunt amet in in mollit mollit in dolor.</p>
+	<div>{{ data }}</div>
 </template>
+<script setup>
+	const {data} = await useFetch('/api/ninja')
+</script>
 <style scoped>
 	h2 {
 		margin-bottom: 20px;
---
+++ server/api/ninja.js
@@ -0,0 +1,12 @@
+export default defineEventHandler(async (event) => {
+	//handle query params
+	// const {name} = getQuery(event)
+
+	//handle post data
+	// const {age} = await readBody(event)
+
+	//api call with private key
+	const {data} = await $fetch('https://api.currencyapi.com/v3/latest?apikey=4FP5Wsl06fRFBNrYkqhhsCxCpaM2fBBwQCILFEAy')
+
+	return data
+})

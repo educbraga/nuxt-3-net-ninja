8d869bde3cfa8a46868f2dd1f87e28638d555b73
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-16 23:37:30 -0300

    adiciona pagina de erro

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -5,10 +5,14 @@
 </template>
 <script setup>
 	const { id } = useRoute().params
-
 	const uri = `https://fakestoreapi.com/products/${id}`
 
+	//fetch the product
 	const {data: product} = await useFetch(uri)
+
+	if(!product.value){
+		throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true})
+	}
 
 	definePageMeta({
 		layout: 'products'
